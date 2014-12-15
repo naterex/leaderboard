45a05b03bb9f7ed1f3e0c318e8f8755d7384655c
Nate <primesitemedia@gmail.com>
2014-12-15 15:21:01 +0800

    add class=selected helper to change <li> when dbl clicked

--- leaderboard-meteor.js
+++ leaderboard-meteor.js
@@ -1,19 +1,30 @@
 // Create a MongoDB Collection
 PlayersList = new Mongo.Collection('players');
-// console.log("PlayersList created");
 
 if(Meteor.isClient){
   Template.leaderboard.helpers({
     // helpers go here
     'player': function(){
       return PlayersList.find()
+    },
+    'selectedClass': function(){
+      var playerID = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerID == selectedPlayer){
+        return "selected"
+      }
     }
   });
   Template.leaderboard.events({
     // events go here
     'dblclick .player': function(){
-      Session.set('selectedPlayer', 'session value test');
-      console.log("You clicked a .player element");
+      var playerID = this._id;
+      Session.set('selectedPlayer', playerID);
+      var selectedPlayer = Session.get('selectedPlayer');
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
     }
   });
 }
