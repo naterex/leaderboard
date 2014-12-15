3150a486113dcd34a24adae8f7f4d3b9e74f53fd
Nate <primesitemedia@gmail.com>
2014-12-15 17:35:51 +0800

    add publish function to isServer, add subscribe function to isClient

--- leaderboard-meteor.js
+++ leaderboard-meteor.js
@@ -1,63 +1,128 @@
+
 // Create a MongoDB Collection
 PlayersList = new Mongo.Collection('players');
 
+// Code that only runs on the client (within the web browser)
 if(Meteor.isClient){
+
+  // "Catch" the selection of data from the Meteor.publish function
+  Meteor.subscribe('thePlayers');
+
+  // Helper functions execute code within templates
   Template.leaderboard.helpers({
-    // helpers go here
     'player': function(){
+
+      // Get the ID of the current user
       var currentUserId = Meteor.userId();
-      return PlayersList.find({createdBy: currentUserId}, {sort: { score: -1, name: 1 } })
+
+      // Retrieve data that belongs to the current user
+      return PlayersList.find({}, {sort: {score: -1, name: 1}});
+
     },
     'selectedClass': function(){
-      var playerID = this._id;
-      var selectedPlayer = Session.get('selectedPlayer');
-      if (playerID == selectedPlayer){
-        return "selected"
-      }
+
+        // Get the ID of the player being iterated through
+        var playerId = this._id;
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+         // Do these IDs match?
+        if(playerId == selectedPlayer){
+
+            // Return a CSS class
+            return "selected"
+
+        }
+
     },
     'showSelectedPlayer': function(){
+
+      // Get the ID of the player that's been clicked
       var selectedPlayer = Session.get('selectedPlayer');
+
+      // Retrieve a single document from the collection
       return PlayersList.findOne(selectedPlayer)
+
     }
   });
+
+  // Events trigger code when certain actions are taken
   Template.leaderboard.events({
-    // events go here
-    'dblclick .player': function(){
-      var playerID = this._id;
-      Session.set('selectedPlayer', playerID);
-      var selectedPlayer = Session.get('selectedPlayer');
-      // console.log(selectedPlayer)
-    },
-    'click .increment': function(){
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, { $inc: { score: 5 } });
-    },
-    'click .decrement': function(){
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, { $inc: { score: -5 } });
-    },
-    'click .remove': function(){
-      var selectedPlayer = Session.get('selectedPlayer');
-      confirm("Are you sure u want to remove this player?");
-      PlayersList.remove(selectedPlayer);
-    }
+      'click .player': function(){
+
+          // Retrieve the unique ID of the player that's been clicked
+          var playerId = this._id;
+
+          // Create a session to store the unique ID of the clicked player
+          Session.set('selectedPlayer', playerId);
+
+      },
+      'click .increment': function(){
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        // Update a document and increment the score field by 5
+        PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+
+      },
+      'click .decrement': function(){
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        // Update a document and decrement the score field by 5
+        PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+
+      },
+      'click .remove': function(){
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        // Remove a document from the collection
+        PlayersList.remove(selectedPlayer);
+
+      }
   });
+
   Template.addPlayerForm.events({
     'submit form': function(event){
-      event.preventDefault();
-      var playerNameVar = event.target.playerName.value;
-      var playerScore = Number(event.target.playerScore.value);
-      var currentUserId = Meteor.userId();
-      PlayersList.insert({
-        name: playerNameVar,
-        score: playerScore,
-        createdBy: currentUserId
-      });
-      event.target.playerName.value = '';
+
+        // Prevent the browser from applying default behaviour to the form
+        event.preventDefault();
+
+        // Get the value from the "playerName" text field
+        playerNameVar = event.target.playerName.value;
+
+        // Get the ID of the current user
+        var currentUserId = Meteor.userId();
+
+        // Insert the new player into the collection
+        PlayersList.insert({
+          name: playerNameVar,
+          score: 0,
+          createdBy: currentUserId
+        });
+
     }
   });
+
 }
 
+// Code that only runs on the server (where the application is hosted)
 if(Meteor.isServer){
+
+  // Transmit a selection of data into the ether
+  Meteor.publish('thePlayers', function(){
+
+    // Get the ID of the current user
+    var currentUserId = this.userId;
+
+    // Return players "owned" by the current user
+    return PlayersList.find({createdBy: currentUserId})
+
+  });
 
 }
